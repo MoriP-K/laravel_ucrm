76dd50bff08e0484c3d06c865c1e7b758a409317
MoriP-K <kohei.moriyama426@gmail.com>
2024-10-01 17:56:30 +0900

    added CRUD process

--- database/seeders/ItemSeeder.php
+++ database/seeders/ItemSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class ItemSeeder extends Seeder
 {
@@ -14,6 +16,22 @@ class ItemSeeder extends Seeder
      */
     public function run()
     {
-        //
+      DB::table('items')->insert([
+				[
+					'name' => 'cut',
+					'memo' => 'cut detail',
+					'price' => 6000,
+				],
+				[
+					'name' => 'color',
+					'memo' => 'color detail',
+					'price' => 8000,
+				],
+				[
+					'name' => 'perm',
+					'memo' => 'perm detail (included cut)',
+					'price' => 13000,
+				],
+			]);
     }
 }
